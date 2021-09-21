c6e0fb652e7a7d966d6136dc1818c0974f7f467b
Aegonek <dominikjachymczyk@gmail.com>
2021-09-21 19:13:33 +0200

    Fixed .csproj.

--- SpellEdits/CauseFear.cs
+++ SpellEdits/CauseFear.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kingmaker.Blueprints;
 
 namespace UnboundedArcana
 {
@@ -10,6 +11,7 @@ namespace UnboundedArcana
     {
         static void CauseFear()
         {
+            var foo = ResourcesLibrary.TryGetBlueprint("bd81a3931aa285a4f9844585b5d97e51")
         }
     }
 }
---
+++ Utilities/BlueprintUtilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.Blueprints;
+
+namespace UnboundedArcana.Utilities
+{
+    static class BlueprintUtilities
+    {
+        public static BlueprintGuid CreateGuid(string raw) => new BlueprintGuid(new Guid(raw));
+    }
+}
