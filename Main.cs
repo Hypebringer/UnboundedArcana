9aa657a5a18a179dd9da40b5ed7200f990a70c4a
Aegonek <dominikjachymczyk@gmail.com>
2021-09-21 18:53:25 +0200

    First commit

---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace UnboundedArcana
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger { get; set; }
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+            return true;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Unbounded Arcana
+
+Make low level magic great again.
+
+An attempt to make no spell useless in late game. Make enchanter wizard usable. Cantrips scale.
